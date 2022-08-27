d56202b17a33f7318e398808f0fcacd454ced5a3
samanenasiri <96475017+samanenasiri@users.noreply.github.com>
2022-08-27 12:29:47 +0430

    Add files via upload

---
+++ per_to_Eng_num.py
@@ -0,0 +1,9 @@
+my_dict={'۰':'0','۱':'1','۲':'2','۳':'3','۴':'4','۵':'5','۶':'6','۷':'7','۸':'8','۹':'9'}
+num=list("۰۱۲۳۴۵۶'۷۸۹")
+for indexes in range(len(num)):
+    for key,value in my_dict.items():
+        if num[indexes]==key  :
+            num[indexes]=value
+        else:
+            continue
+print("".join(num))
